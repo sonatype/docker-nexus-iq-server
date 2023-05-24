c541d6c762db7e243ed64367c0f67d2e6866f27f
Agustin Martinez <agustin.martinez@sonaype.com>
2023-05-24 10:03:20 -0700

    CLM-22746 test groovy load

--- src/test/com/sonatype/docker/nexus/iq/server/DockerNexusIqServerTest.groovy
+++ src/test/com/sonatype/docker/nexus/iq/server/DockerNexusIqServerTest.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2017-present Sonatype, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.sonatype.docker.nexus.iq.server
 
 @Library(['jenkins-shared']) _
@@ -5,7 +20,7 @@ import com.sonatype.jenkins.shared.Expectation
 
 public final class DockerNexusIqServerTest
 {
-  public static Expectation[] nexusIqServerontainerExpectations() {
+  public static Expectation[] nexusIqServerContainerExpectations() {
     return [
       new Expectation('nexus-group', 'grep', '^nexus: /etc/group', 'nexus:x:1000:'),
       new Expectation('nexus-user', 'grep', '^nexus: /etc/passwd', 'nexus:x:1000:1000:Nexus IQ user:/opt/sonatype/nexus-iq-server:/bin/false'),
