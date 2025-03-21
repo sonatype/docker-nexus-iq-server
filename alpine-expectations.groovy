f646d903df9008930c7387a3364fcfe977c57782
Ross Pokorny <rpokorny@sonatype.com>
2025-03-21 13:53:12 -0400

    Add curl to alpine expectations - CLM-34331

--- alpine-expectations.groovy
+++ alpine-expectations.groovy
@@ -28,12 +28,13 @@ def containerExpectations() {
     new Expectation('stderr-log', 'test', '-f /var/log/nexus-iq-server/stderr.log | echo $?', '0'),
     new Expectation('home-directory', 'ls', '-la /opt/sonatype | grep nexus-iq-server | awk \'\$9 !~ /^\\.*$/{print \$1,\$3,\$4,\$9}\'', 'drwxr-sr-x nexus nexus nexus-iq-server'),
     new Expectation('start-script', 'test', '-f /opt/sonatype/nexus-iq-server/start.sh | echo $?', '0'),
-    new Expectation('start-script-has-java-opts', 'grep', '\'JAVA_OPTS\' /opt/sonatype/nexus-iq-server/start.sh | echo $?', '0'), 
-    new Expectation('work-directory', 'ls', '-la / | grep sonatype-work | awk \'\$9 !~ /^\\.*$/{print \$1,\$3,\$4,\$9}\'', 'drwxr-xr-x nexus nexus sonatype-work'), 
-    new Expectation('data-directory', 'test', '-d /sonatype-work/data | echo $?', '0'), 
-    new Expectation('config-directory', 'ls', '-la /etc | grep nexus-iq-server | awk \'\$9 !~ /^\\.*$/{print \$1,\$3,\$4,\$9}\'', 'drwxr-xr-x nexus nexus nexus-iq-server'), 
-    new Expectation('config-file', 'test', '-f /etc/nexus-iq-server | echo $?', '0') 
-  ]     
+    new Expectation('start-script-has-java-opts', 'grep', '\'JAVA_OPTS\' /opt/sonatype/nexus-iq-server/start.sh | echo $?', '0'),
+    new Expectation('work-directory', 'ls', '-la / | grep sonatype-work | awk \'\$9 !~ /^\\.*$/{print \$1,\$3,\$4,\$9}\'', 'drwxr-xr-x nexus nexus sonatype-work'),
+    new Expectation('data-directory', 'test', '-d /sonatype-work/data | echo $?', '0'),
+    new Expectation('config-directory', 'ls', '-la /etc | grep nexus-iq-server | awk \'\$9 !~ /^\\.*$/{print \$1,\$3,\$4,\$9}\'', 'drwxr-xr-x nexus nexus nexus-iq-server'),
+    new Expectation('config-file', 'test', '-f /etc/nexus-iq-server | echo $?', '0'),
+    new Expectation('curl', 'curl --version | echo $?', '0')
+  ]
 }
 
-return this; 
+return this;
