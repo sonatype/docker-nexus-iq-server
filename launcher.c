54b5325fe8b8c6d65b038c0cb9189d602277bf04
Ross Pokorny <rpokorny@sonatype.com>
2026-04-20 10:23:34 -0400

    Improve formatting and free memory earlier

--- launcher.c
+++ launcher.c
@@ -32,49 +32,51 @@
  * Note that Java itself has no equivalent of execvp, which is why this is written in C.
  */
 int main(void) {
-    char *args[MAX_ARGS];
-    int arg_count = 0;
+  char *args[MAX_ARGS];
+  int arg_count = 0;
 
-    // Redirect stderr to log file
-    int stderr_fd = open("/var/log/nexus-iq-server/stderr.log",
-                         O_WRONLY | O_CREAT | O_APPEND, 0644);
-    if (stderr_fd >= 0) {
-        dup2(stderr_fd, STDERR_FILENO);
-        close(stderr_fd);
+  // Redirect stderr to log file
+  int stderr_fd = open("/var/log/nexus-iq-server/stderr.log",
+                       O_WRONLY | O_CREAT | O_APPEND, 0644);
+  if (stderr_fd >= 0) {
+    dup2(stderr_fd, STDERR_FILENO);
+    close(stderr_fd);
+  }
+
+  // Build argument list
+  args[arg_count++] = "java";
+  args[arg_count++] = "@/opt/sonatype/nexus-iq-server/jvm.options";
+
+  // Parse JAVA_OPTS - simple whitespace splitting (matches unquoted $JAVA_OPTS in bash)
+  char *opts_copy = NULL;
+  char *java_opts = getenv("JAVA_OPTS");
+  if (java_opts != NULL && java_opts[0] != '\0') {
+    opts_copy = strdup(java_opts);
+
+    if (opts_copy == NULL) {
+      fprintf(stderr, "Warning: failed to allocate memory for JAVA_OPTS, ignoring: %s\n", java_opts);
+    } else {
+      char *token = strtok(opts_copy, " \t\n");
+      while (token != NULL && arg_count < MAX_ARGS - 5) {
+        args[arg_count++] = token;
+        token = strtok(NULL, " \t\n");
+      }
     }
 
-    // Build argument list
-    args[arg_count++] = "java";
-    args[arg_count++] = "@/opt/sonatype/nexus-iq-server/jvm.options";
-
-    // Parse JAVA_OPTS - simple whitespace splitting (matches unquoted $JAVA_OPTS in bash)
-    char *opts_copy = NULL;
-    char *java_opts = getenv("JAVA_OPTS");
-    if (java_opts != NULL && java_opts[0] != '\0') {
-        opts_copy = strdup(java_opts);
-        if (opts_copy == NULL) {
-            fprintf(stderr, "Warning: failed to allocate memory for JAVA_OPTS, ignoring: %s\n", java_opts);
-        } else {
-            char *token = strtok(opts_copy, " \t\n");
-            while (token != NULL && arg_count < MAX_ARGS - 5) {
-                args[arg_count++] = token;
-                token = strtok(NULL, " \t\n");
-            }
-        }
-    }
-
-    // Add remaining arguments
-    args[arg_count++] = "-jar";
-    args[arg_count++] = "/opt/sonatype/nexus-iq-server/nexus-iq-server.jar";
-    args[arg_count++] = "server";
-    args[arg_count++] = "/etc/nexus-iq-server/config.yml";
-    args[arg_count] = NULL;
-
-    // Exec - replace this process with java
-    execvp("java", args);
-
-    // If we get here, exec failed
-    perror("execvp failed");
     free(opts_copy);
-    return 1;
+  }
+
+  // Add remaining arguments
+  args[arg_count++] = "-jar";
+  args[arg_count++] = "/opt/sonatype/nexus-iq-server/nexus-iq-server.jar";
+  args[arg_count++] = "server";
+  args[arg_count++] = "/etc/nexus-iq-server/config.yml";
+  args[arg_count] = NULL;
+
+  // Exec - replace this process with java
+  execvp("java", args);
+
+  // If we get here, exec failed
+  perror("execvp failed");
+  return 1;
 }
